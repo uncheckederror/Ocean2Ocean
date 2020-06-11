3533329da2329b9f533428c1ae344ed876531d80
Thomas Ryan <uncheckederror@users.noreply.github.com>
2020-06-11 16:14:51 -0700

    Added local App Insights.

--- Ocean2Ocean.Mvc/Startup.cs
+++ Ocean2Ocean.Mvc/Startup.cs
@@ -26,6 +26,7 @@ namespace Ocean2Ocean
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
