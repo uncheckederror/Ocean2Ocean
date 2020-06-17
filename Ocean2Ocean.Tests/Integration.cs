20566b2222f8a6fbc948044226d440dab439defb
Thomas Ryan <uncheckederror@users.noreply.github.com>
2020-06-17 13:43:18 -0700

    Added a test for the data access method to sum steps by day on the leadersboard page.

--- Ocean2Ocean.Tests/Integration.cs
+++ Ocean2Ocean.Tests/Integration.cs
@@ -117,6 +117,20 @@ namespace Ocean2Ocean.Tests
         }
 
         [Fact]
+        public async Task GetTotalStepsByDay()
+        {
+            var results = await Step.GetByJourneyAsync("test", _azureSql);
+            Assert.NotNull(results);
+            Assert.True(results.Any());
+
+            var journey = results.FirstOrDefault();
+
+            var leaders = await Leaderboard.GetTotalStepsByDayAsync(journey.JourneyName, _azureSql);
+            Assert.NotNull(leaders);
+            Assert.True(leaders.Any());
+        }
+
+        [Fact]
         public async Task AddAnEntry()
         {
             var entry = new Step
